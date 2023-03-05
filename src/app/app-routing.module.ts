281f81ad73c435b0472102581ec24b631ebabbca
DESKTOP-DTFO15Q\Akiba <kredynets.a@gmail.com>
2023-03-05 16:42:32 +0200

    admin-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,8 +9,10 @@ const routes: Routes = [
       {path: '', redirectTo: '/' ,pathMatch: 'full'},
       {path: '', component: HomePageComponent},
       {path: 'product/:id', component: ProductPageComponent}
-
-    ]}
+    ]},
+  {
+    path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  }
 ];
 
 @NgModule({
