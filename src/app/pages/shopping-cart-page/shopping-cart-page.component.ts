324ee160515c29fefec97917ec50508a84cbbfe2
DESKTOP-DTFO15Q\Akiba <kredynets.a@gmail.com>
2023-03-14 00:27:47 +0200

    cart

--- src/app/pages/shopping-cart-page/shopping-cart-page.component.ts
+++ src/app/pages/shopping-cart-page/shopping-cart-page.component.ts
@@ -1,13 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {ColDef} from "ag-grid-community";
-import {ImgCellRendererComponent} from "../../shared/components/img-cell-renderer/img-cell-renderer.component";
-import {DescCellRendererComponent} from "../../shared/components/desc-cell-renderer/desc-cell-renderer.component";
-import {QtyCellRendererComponent} from "../../shared/components/qty-cell-renderer/qty-cell-renderer.component";
-import {
-  ActionsCellRendererComponent
-} from "../../shared/components/actions-cell-renderer/actions-cell-renderer.component";
-import {TotalCellRendererComponent} from "../../shared/components/total-cell-renderer/total-cell-renderer.component";
 import {CartService} from "../../services/cart.service";
+import {Product} from "../../product.interface";
 
 
 @Component({
@@ -16,62 +9,18 @@ import {CartService} from "../../services/cart.service";
   styleUrls: ['./shopping-cart-page.component.scss']
 })
 export class ShoppingCartPageComponent implements OnInit {
-  gridApi: any;
-  rowData: any;
-  editType: 'fullRow' = 'fullRow';
-  columnDefs: ColDef[] = [
-    {
-      field: 'item',
-      cellRenderer: ImgCellRendererComponent,
-      width: 200,
-      autoHeight: true
-    },
-    {
-      field: 'name',
-      cellRenderer: DescCellRendererComponent,
-    },
-    {
-      field: 'qty',
-      cellRenderer: QtyCellRendererComponent,
-      cellRendererParams: {
-      plus: (productId: number) => {
-        console.log(productId, 'Product ID')
-
-      },
-    //     // minus: (res: number) => {
-    //     //   console.log(res)
-    //     // }
-      }
-    },
-    {
-      field: 'price',
-    },
-    {
-      field: 'total',
-      valueGetter: 'data.quantity * data.price'
-    },
-    {
-      field: 'actions',
-      cellRenderer: ActionsCellRendererComponent
-    }
-  ];
-
-
+  products: Product[] = [];
 
   constructor(private cartService: CartService) {}
-  onGridReady(params: any) {
-    this.gridApi = params.api;
-    params.api.sizeColumnsToFit();
-  }
 
   ngOnInit(): void {
     this.cartService.getProducts().subscribe((res) => {
-      this.rowData = res
+      this.products = res
     })
   }
 
-  // rowData = [
-  //   {id: 1, productLink:'https://material.angular.io/assets/img/examples/shiba2.jpg', img: 'https://material.angular.io/assets/img/examples/shiba2.jpg', name: 'Product title', desc:'The Shiba Inu is the smallest of the six original.', qty: '1', price: '35',  },
-  //   { id: 2, productLink:'https://material.angular.io/assets/img/examples/shiba2.jpg', img: 'https://material.angular.io/assets/img/examples/shiba2.jpg', name: 'Product title', desc:'The Shiba Inu is the smallest of the six original.', qty: '1', price: '35',  },
-  // ];
+  clearCart() {
+    this.cartService.clearCart();
+  }
+
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -18,8 +18,56 @@ export class CartService {
     this.productList.next(product);
   }
   addToCart(product: Product) {
-    this.cartList.push(product);
+
+    if (!this.cartList.includes(product)) {
+      this.cartList.push(product)
+      this.productList.next(this.cartList);
+    }
+    // this.cartList.push(product);
     this.productList.next(this.cartList);
+
+  }
+
+  deleteProduct(productId: number) {
+    this.cartList.map((p: Product, i) => {
+      if (productId === p.id) {
+        this.cartList.splice(i, 1)
+      }
+    })
+    this.productList.next(this.cartList)
+  }
+
+  clearCart() {
+    this.cartList = [];
+    this.productList.next(this.cartList)
+  }
+
+  checkDuplicates(product: Product) {
+    this.cartList.map((p: Product, i) => {
+      if (product.id === p.id) {
+        this.cartList[i].quantity =+1
+        console.log(this.cartList)
+        this.productList.next(this.cartList);
+        return
+      }
+    })
+
+    // this.cartList.map((p: Product, i) => {
+    //   if (product.id === p.id) {
+    //     // const prodQty = (this.cartList[i].quantity ?? 0 ) +1
+    //     const prodQty = this.cartList[i].quantity
+    //     console.log(prodQty , 'product qty')
+    //     this.productList.next(this.cartList);
+    //   }
+    // })
+  }
+  updateProductQty(productId: number, qty: number) {
+    this.cartList.map((p:Product, i) => {
+      if (p.id === productId) {
+        this.cartList[i].quantity = qty
+        this.productList.next(this.cartList)
+      }
+    })
   }
 
 }
