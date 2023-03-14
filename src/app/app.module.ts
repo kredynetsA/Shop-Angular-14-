d7f76ed54e11fece65eef5037b5900dca0302b7e
DESKTOP-DTFO15Q\Akiba <kredynets.a@gmail.com>
2023-03-14 18:56:12 +0200

    remove-ag-grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,13 +15,7 @@ import {MatCardModule} from "@angular/material/card";
 import {AdminModule} from "./admin/admin.module";
 import { ShoppingCartPageComponent } from './pages/shopping-cart-page/shopping-cart-page.component';
 import { CartProductItemComponent } from './shared/components/cart-product-item/cart-product-item.component';
-import {AgGridModule} from "ag-grid-angular";
-import { ImgCellRendererComponent } from './shared/components/img-cell-renderer/img-cell-renderer.component';
-import { DescCellRendererComponent } from './shared/components/desc-cell-renderer/desc-cell-renderer.component';
-import { QtyCellRendererComponent } from './shared/components/qty-cell-renderer/qty-cell-renderer.component';
 import {FormsModule} from "@angular/forms";
-import { ActionsCellRendererComponent } from './shared/components/actions-cell-renderer/actions-cell-renderer.component';
-import { TotalCellRendererComponent } from './shared/components/total-cell-renderer/total-cell-renderer.component';
 import {HttpClientModule} from "@angular/common/http";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 import {MatSnackBarModule} from "@angular/material/snack-bar";
@@ -35,11 +29,6 @@ import {MatSnackBarModule} from "@angular/material/snack-bar";
     ProductComponent,
     ShoppingCartPageComponent,
     CartProductItemComponent,
-    ImgCellRendererComponent,
-    DescCellRendererComponent,
-    QtyCellRendererComponent,
-    ActionsCellRendererComponent,
-    TotalCellRendererComponent
   ],
   imports: [
     BrowserModule,
@@ -50,7 +39,6 @@ import {MatSnackBarModule} from "@angular/material/snack-bar";
     MatButtonModule,
     MatCardModule,
     AdminModule,
-    AgGridModule,
     FormsModule,
     HttpClientModule,
     MatProgressSpinnerModule,
