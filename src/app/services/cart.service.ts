87c101a9c483dd053c0a915726ed5dc366bb5140
DESKTOP-DTFO15Q\Akiba <kredynets.a@gmail.com>
2023-03-19 19:42:38 +0200

    remove-console-log

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -20,7 +20,6 @@ export class CartService {
     this.productList.next(product);
   }
   addToCart(product: Product) {
-    console.log(product)
     if (!this.cartList.includes(product)) {
       product.quantity = 1
       product.totalPrice = product.price
